ae9bf80a566ac4ecc2eff50b6f28280e45fda078
Edson Alcala <edson_alcala@msn.com>
2017-12-30 01:02:49 +0000

    added sucess message

--- src/pages/equipment-detail/equipment-detail.ts
+++ src/pages/equipment-detail/equipment-detail.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {  NavController, NavParams, LoadingController} from 'ionic-angular';
+import { NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
+import { ToastServiceProvider } from '../../providers/toast-service/toast-service';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-equipment-detail',
@@ -7,29 +9,57 @@ import {  NavController, NavParams, LoadingController} from 'ionic-angular';
 })
 export class EquipmentDetailPage {
   equipment: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController) {
-      this.equipment = this.navParams.get('equipment');
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController,
+    private toastService: ToastServiceProvider,
+    private alertCtrl: AlertController) {
+    this.equipment = this.navParams.get('equipment');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EquipmentDetailPage');
   }
 
-  requestEquipment(){
-     //debugger;
+  requestEquipment() {
+    //debugger;
     //TODO: still need to add logic and get the parameter of the program id
-    this.loading()
+    this.loading();
   }
-  
-  loading() {
-  let loading = this.loadingCtrl.create({
-    spinner: 'crescent',
-    content: 'Requesting equipment',
-    duration: 3000,
-    showBackdrop: true,
-    cssClass:'loader',
-  });
 
-  loading.present();
+  presentAlert() {
+    let alert = this.alertCtrl.create({
+      subTitle: 'Your request has been sent',
+      buttons: [
+        {
+          text: 'Ok',       
+          handler: () => {
+            this.navCtrl.push(HomePage);
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
+  loading() {
+    let loading = this.loadingCtrl.create({
+      spinner: 'iOS',
+      content: 'Requesting equipment',
+      duration: 3000,
+      showBackdrop: true,
+      cssClass: 'loader',
+    });
+
+    loading.onDidDismiss(() => {
+      console.log('Dismissed loading');
+        this.presentAlert();
+      	// this.toastService.presentToast("A new program has been released", () =>{
+		 		// navCtrl.push(DetailsPage);
+			  // });//puedo usar los toasters para los mensajes de exito	
+    });
+
+    loading.present();
   }
 }
--- src/pages/program-detail/program-detail.ts
+++ src/pages/program-detail/program-detail.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
-import {  NavController, NavParams } from 'ionic-angular';
+import {  NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ObsidianApiServiceProvider } from '../../providers/obsidian-api-service/obsidian-api-service';
 import { Observable } from 'rxjs/Observable';
+import { Web3ServiceProvider } from '../../providers/web3-service/web3-service';
 
 @Component({
   selector: 'page-program-detail',
   templateUrl: 'program-detail.html',
 })
 export class ProgramDetailPage {
-  programTest: any;
+  program: any;
   ipfsHash: string;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public obsidianApi: ObsidianApiServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    public obsidianApi: ObsidianApiServiceProvider,
+    private web3Service: Web3ServiceProvider,
+    public loadingCtrl: LoadingController) {
     //this.program = this.navParams.get('program');
     //debugger;
     // if(!this.program){
     //     //this.ipfsHash = this.navParams.get("ipfsHash");
         
     //   }
-      this.programTest = obsidianApi.getProgram("Qma9deZjtNs5Dy63wdQgiaGqJcNKNveC7PrrwTCKMKYcqr");//this.ipfsHash);
+      this.program = obsidianApi.getProgram("Qma9deZjtNs5Dy63wdQgiaGqJcNKNveC7PrrwTCKMKYcqr");//this.ipfsHash);
    
   }
 
@@ -26,7 +30,23 @@ export class ProgramDetailPage {
   }
 
   applyToProgram(){
+    this.loading();
+    this.web3Service.applyForProgram(this.program.programId)
+      .then((result)=>{
+        //show sucesss message, an alert maybe, and you click and see the details
+      })
     //debugger;
     //TODO: still need to add logic and get the parameter of the program id
   }
+
+  loading() {
+    let loading = this.loadingCtrl.create({
+      spinner: 'crescent',
+      content: 'Requesting equipment',     
+      showBackdrop: true,
+      cssClass:'loader',
+    });
+  
+    loading.present();
+    }
 }
