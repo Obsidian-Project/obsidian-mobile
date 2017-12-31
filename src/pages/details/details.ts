f58e9068456746d51864753f1bf3fdebc15c457e
Edson Alcala <edson_alcala@msn.com>
2017-12-31 04:37:58 +0000

    added details

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import {  NavController, NavParams } from 'ionic-angular';
+import { Web3ServiceProvider } from '../../providers/web3-service/web3-service';
+import { ObsidianApiServiceProvider } from '../../providers/obsidian-api-service/obsidian-api-service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-details',
   templateUrl: 'details.html',
 })
 export class DetailsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  balance: any;
+  equipments: Observable<any>;
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    private web3Service: Web3ServiceProvider,
+    private obsidianServiceAPI: ObsidianApiServiceProvider) {
+      web3Service.setupSmartContractInfo()
+			.then((info: any) => {
+				this.web3Service.getMyBalance().then((result) =>{				
+          this.balance = result;
+          this.equipments =  this.obsidianServiceAPI.getMyEquipments();
+				})
+			}).catch((error) => {
+				console.log(error);
+			});
   }
 
   ionViewDidLoad() {
